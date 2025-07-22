c4b8d6a5f501599224d322b4fd26db4b9f67c50e
veronosmani <veron.osmani@gmail.com>
2025-07-22 09:28:59 +0200

    style fix

--- DapperDemo/Models/Company.cs
+++ DapperDemo/Models/Company.cs
@@ -1,4 +1,6 @@
-﻿namespace DapperDemo.Models
+﻿using System.ComponentModel;
+
+namespace DapperDemo.Models
 {
     public class Company
     {
@@ -7,7 +9,7 @@
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        
+        [DisplayName("Postal Code")]
         public string PostalCode { get; set; }
     }
 }
